3cc9945314547e5aa0902808c72998e36be09a3b
Vikash Vishwakarma <vv6330474@gmail.com>
2026-07-06 15:49:01 +0000

    Set custom vikash logo as favicon

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const path = require('path');
 
 const PORT = 5000;
 
+const mimeTypes = {
+  '.html': 'text/html',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.ico':  'image/x-icon',
+};
+
 http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
+  const url = req.url === '/' ? '/index.html' : req.url;
+  const filePath = path.join(__dirname, url);
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'text/plain';
+
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(500);
-      res.end('Error loading page');
+      res.writeHead(404);
+      res.end('Not found');
       return;
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': contentType });
     res.end(data);
   });
 }).listen(PORT, '0.0.0.0', () => {
